703d7614ee84fe38980012c2f582160aae9c19b5
Yash Khakhar <yashkhakhkhar455@gmail.com>
2025-06-01 13:08:47 +0530

    utility project

---
+++ MarketMatrix_Utility/EmailSender.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Net.Mail;
+using System.Net;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Identity.UI.Services;
+
+namespace MarketMatrix_Utility
+{
+	public class EmailSender : IEmailSender
+	{
+        private readonly ILogger<EmailSender> _logger;
+        private readonly IConfiguration _config;
+
+        public EmailSender(ILogger<EmailSender> logger, IConfiguration config) { 
+            _logger = logger;
+            _config = config;
+        }
+
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+		{
+            var smtpClient = new SmtpClient("smtp.gmail.com")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential(
+                _config["EmailSettings:FromEmail"],
+                _config["EmailSettings:AppPassword"]
+            ),
+                EnableSsl = true,
+            };
+
+            var mailMessage = new MailMessage
+            {
+                From = new MailAddress(_config["EmailSettings:FromEmail"]),
+                Subject = subject,
+                Body = htmlMessage,
+                IsBodyHtml = true,
+            };
+
+            mailMessage.To.Add(email);
+
+            return smtpClient.SendMailAsync(mailMessage);
+        }
+	}
+}
---
+++ MarketMatrix_Utility/SD.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace MarketMatrix_Utility
+{
+	public static class SD
+	{
+		public const string Role_Customer = "Customer";
+
+		public const string Role_Admin = "Admin";
+
+    }
+}
